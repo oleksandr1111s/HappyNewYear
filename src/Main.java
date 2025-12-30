da5ef7906649bee22abeef2653b41590d93dc2fb
Student_1 <codecat.ua@gmail.com>
2025-12-30 19:37:56 +0200

    Add new user

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        User Oleksandr = new User(121212, "Zhdanov", 5000);
-        User Timur = new User(456121, "Hladiy", 10);
-        User Vova = new User(532134, "Kostyk", 4000);
-        User Pasha = new User(656564, "Kotleta", 1000000);
-        System.out.println("У користувача з прізвищем " + "Zhdanov" + " і ID " + 121212 + " з балансом " + 5000 + ".");
-        System.out.println("У користувача з прізвищем " + "Hladiy" + " і ID " + 456121 + " з балансом " + 10 + ".");
-        System.out.println("У користувача з прізвищем " + "Kostyk" + " і ID " + 532134 + " з балансом " + 4000 + ".");
-        System.out.println("У користувача з прізвищем " + "Kotleta" + " і ID " + 656564 + " з балансом " + 1000000 + ".");
+        User Oleksandr = new User(121212, "Oleksandr", "Zhdanov", 5000);
+        User Timur = new User(456121,"Timur", "Hladiy", 10);
+        User Vova = new User(532134,"Vova", "Kostyk", 4000);
+        User Pasha = new User(656564,"Pasha", "Kotleta", 1000000);
+        System.out.println("У користувача з ім'ям " + "Oleksandr" + " та прізвищем " + "Zhdanov" + " і ID " + 121212 + " з балансом " + 5000 + ".");
+        System.out.println("У користувача з ім'ям " + "Timur" + " та прізвищем " + "Hladiy" + " і ID " + 456121 + " з балансом " + 10 + ".");
+        System.out.println("У користувача з ім'ям " + "Vova" + " та прізвищем " + "Kostyk" + " і ID " + 532134 + " з балансом " + 4000 + ".");
+        System.out.println("У користувача з ім'ям " + "Pasha" + " та прізвищем " + "Kotleta" + " і ID " + 656564 + " з балансом " + 1000000 + ".");
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,10 +1,12 @@
 public class User {
     public int ID;
+    public  String firstName;
     public String lastName;
     public double money;
 
-    public User(int ID, String lastName, double money) {
+    public User(int ID, String firstName, String lastName, double money) {
         this.ID = ID;
+        this.firstName = firstName;
         this.lastName = lastName;
         this.money = money;
     }
